139497978e6b804de1295e9a95ce8eb3f9e5d83c
刘辉(20020013) <20020013@suning.com>
2020-10-10 19:08:15 +0800

    引入 babel-polyfill  解决兼容问题

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'babel-polyfill';
 import './index.less';
 
 import { store } from './store';
