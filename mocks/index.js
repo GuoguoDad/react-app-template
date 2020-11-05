251d3dcb6f556dc512ca63643be84b2e959e8365
刘辉20020013 <20020013@suning.com>
2020-11-05 15:49:55 +0800

    readme

--- mocks/index.js
+++ mocks/index.js
@@ -1,18 +1,24 @@
-const path = require('path')
-const express = require('express')
-const mockjs = require('./express-mockjs')
+const path = require('path');
+const express = require('express');
+const mockjs = require('./express-mockjs');
 
-const port = 8090
-const app = express()
+const port = 8090;
+const app = express();
 
 app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.referer ? req.headers.referer.match(/^\w+\:\/\/[^\/]+/)[0] : '*');
-  res.header("Access-Control-Allow-Headers", "x-requested-with,mode, accept, origin, content-type,authorization,credentials");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("Content-Type", "application/json;charset=utf-8");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    'Access-Control-Allow-Origin',
+    req.headers.referer ? req.headers.referer.match(/^\w+\:\/\/[^\/]+/)[0] : '*',
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-requested-with,mode, accept, origin, content-type,authorization,credentials',
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
-  if ('OPTIONS' == req.method) {
+  if (req.method === 'OPTIONS') {
     res.send(200);
   } else {
     next();
@@ -20,11 +26,11 @@ app.all('*', function (req, res, next) {
 });
 
 app.get('/mock-ui.html', (req, res) => {
-  res.setHeader('content-type', 'text/html')
+  res.setHeader('content-type', 'text/html');
   res.sendFile(path.resolve(__dirname, './doc.html'));
-})
+});
 
-app.use('/api', mockjs(path.join(__dirname, '../mocks')))
+app.use('/api', mockjs(path.join(__dirname, '../mocks')));
 
-console.log(`App Mock Server running  ${port}`)
-app.listen(port)
+console.log(`App Mock Server running  ${port}`);
+app.listen(port);
