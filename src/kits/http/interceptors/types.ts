4306640819f2fb5818cab8dbea7825b911c73b2c
刘辉20020013 <20020013@suning.com>
2020-11-05 14:16:59 +0800

    eslint code prettier

--- src/kits/http/interceptors/types.ts
+++ src/kits/http/interceptors/types.ts
@@ -6,6 +6,4 @@ export type AxiosRequsetInterceptor = [RequestOnFulfilled, OnRejected];
 export type OnFulfilled<V> = (value: V) => AxiosResponse<V> | Promise<AxiosResponse<V>>;
 export type OnRejected = (error: any) => any;
 
-export type RequestOnFulfilled = (
-    value: AxiosRequestConfig
-) => Promise<AxiosRequestConfig> | AxiosRequestConfig;
+export type RequestOnFulfilled = (value: AxiosRequestConfig) => Promise<AxiosRequestConfig> | AxiosRequestConfig;
