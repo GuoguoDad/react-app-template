a08830cfe9b7c912482bbbb2142577037f5442f7
刘辉20020013 <20020013@suning.com>
2020-11-10 20:01:02 +0800

    ~

--- src/pages-demo/counter/counter.tsx
+++ src/pages-demo/counter/counter.tsx
@@ -18,10 +18,6 @@ const Counter = () => {
           onClick={() => {
             batch(() => {
               dispatch(increment());
-              dispatch(increment());
-              dispatch(increment());
-              dispatch(increment());
-              dispatch(increment());
             });
           }}
         >
