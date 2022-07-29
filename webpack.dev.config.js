8f7ff7a84228c4f7ade2c30256ab67594cdc7e36
刘辉20020013 <20020013@suning.com>
2022-07-29 15:18:29 +0800

    feat: update packages webpack-plugin-mock

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.config')
+const WebpackPluginMock = require('webpack-plugin-mock')
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'development',
@@ -7,6 +8,14 @@ module.exports = merge(baseWebpackConfig, {
     app: './src/index.tsx'
   },
   devtool: 'cheap-module-source-map',
+  plugins: [
+    new WebpackPluginMock({
+      apiBasePath: './mock',
+      watch: true,
+      pretty: true,
+      port: 8090
+    }),
+  ],
   devServer: {
     port: 8080,
     host: '0.0.0.0',
